b73eb40cd74a518621749815e000b28d3fa16c51
sdavisde <sdavisde@gmail.com>
2025-04-02 07:21:41 -0500

    modularize nvim config

---
+++ lua/custom/plugins/comments.lua
@@ -0,0 +1,15 @@
+-- Highlight todo, notes, etc in comments
+return {
+  {
+    'folke/todo-comments.nvim',
+    event = 'VimEnter',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    opts = { signs = false },
+  },
+  {
+    'numToStr/Comment.nvim',
+    opts = {
+      -- add any options here
+    },
+  },
+}
---
+++ lua/lazy-plugins.lua
@@ -0,0 +1,61 @@
+-- [[ Configure and install plugins ]]
+--
+--  To check the current status of your plugins, run
+--    :Lazy
+--
+--  To update plugins you can run
+--    :Lazy update
+--
+require('lazy').setup({
+  -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
+  'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
+
+  require 'custom/plugins/autocomplete',
+
+  require 'custom/plugins/comments',
+
+  require 'custom/plugins/format',
+
+  require 'custom/plugins/lint',
+
+  require 'custom/plugins/lsp',
+
+  require 'custom/plugins/mini',
+
+  require 'custom/plugins/navigation',
+
+  require 'custom/plugins/theme',
+
+  require 'custom/plugins/treesitter',
+
+  require 'custom/plugins/which-key',
+
+  -- require 'kickstart.plugins.debug',
+  require 'kickstart.plugins.indent_line',
+  -- require 'kickstart.plugins.lint',
+  require 'kickstart.plugins.autopairs',
+  require 'kickstart.plugins.neo-tree',
+  require 'kickstart.plugins.gitsigns'
+}, {
+  ui = {
+    -- If you are using a Nerd Font: set icons to an empty table which will use the
+    -- default lazy.nvim defined Nerd Font icons, otherwise define a unicode icons table
+    icons = vim.g.have_nerd_font and {} or {
+      cmd = '⌘',
+      config = '🛠',
+      event = '📅',
+      ft = '📂',
+      init = '⚙',
+      keys = '🗝',
+      plugin = '🔌',
+      runtime = '💻',
+      require = '🌙',
+      source = '📄',
+      start = '🚀',
+      task = '📌',
+      lazy = '💤 ',
+    },
+  },
+})
+
+-- vim: ts=2 sts=2 sw=2 et
